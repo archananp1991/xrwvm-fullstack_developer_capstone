cf35d2df6f4760e4bea0d1e0178055c792e75f88
archananp1991 <archananp1991@gmail.com>
2025-03-08 11:44:23 -0500

    module4 finish

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -35,5 +35,5 @@ def initiate():
     ]
     d=0
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'], dealer_id=int(d/3))
+            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'], dealer_id=int(d))
             d=d+1
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
-
+    path(route='add_review',view=views.add_review, name='add_review'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='postreview/<int:dealer_id>',view=TemplateView.as_view(template_name="index.html"), name='postreview'),
+    #path(route='postreview/<int:dealer_id>',view=TemplateView.as_view(template_name="index.html"), name='postreview'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
