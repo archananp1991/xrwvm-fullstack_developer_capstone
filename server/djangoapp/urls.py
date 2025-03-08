543261f01fc3e7cc55b0cf1b67b6da3a02266a48
archananp1991 <archananp1991@gmail.com>
2025-03-08 07:31:23 -0500

    module 5 incomplete

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -20,10 +21,10 @@ urlpatterns = [
     path(route='get_cars', view=views.get_cars, name ='getcars'), 
 
     # path for dealer reviews view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
 
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    
+    path(route='postreview/<int:dealer_id>',view=TemplateView.as_view(template_name="index.html"), name='postreview'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
